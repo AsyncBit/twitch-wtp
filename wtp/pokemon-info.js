579811bedf33eae0330388e9a5100cc4adccbd3e
AsyncBit <102470788+AsyncBit@users.noreply.github.com>
2024-08-22 09:51:58 +0200

    Add files via upload

---
+++ wtp/pokemon-info.js
@@ -0,0 +1,1010 @@
+let pokemonList = {
+  "001": {
+    name: "bulbasaur",
+    pokedex: null,
+  },
+  "002": {
+    name: "ivysaur",
+    pokedex: null,
+  },
+  "003": {
+    name: "venusaur",
+    pokedex: null,
+  },
+  "004": {
+    name: "charmander",
+    pokedex: null,
+  },
+  "005": {
+    name: "charmeleon",
+    pokedex: null,
+  },
+  "006": {
+    name: "charizard",
+    pokedex: null,
+  },
+  "007": {
+    name: "squirtle",
+    pokedex: null,
+  },
+  "008": {
+    name: "wartortle",
+    pokedex: null,
+  },
+  "009": {
+    name: "blastoise",
+    pokedex: null,
+  },
+  "010": {
+    name: "caterpie",
+    pokedex: null,
+  },
+  "011": {
+    name: "metapod",
+    pokedex: null,
+  },
+  "012": {
+    name: "butterfree",
+    pokedex: null,
+  },
+  "013": {
+    name: "weedle",
+    pokedex: null,
+  },
+  "014": {
+    name: "kakuna",
+    pokedex: null,
+  },
+  "015": {
+    name: "beedrill",
+    pokedex: null,
+  },
+  "016": {
+    name: "pidgey",
+    pokedex: null,
+  },
+  "017": {
+    name: "pidgeotto",
+    pokedex: null,
+  },
+  "018": {
+    name: "pidgeot",
+    pokedex: null,
+  },
+  "019": {
+    name: "rattata",
+    pokedex: null,
+  },
+  "020": {
+    name: "raticate",
+    pokedex: null,
+  },
+  "021": {
+    name: "spearow",
+    pokedex: null,
+  },
+  "022": {
+    name: "fearow",
+    pokedex: null,
+  },
+  "023": {
+    name: "ekans",
+    pokedex: null,
+  },
+  "024": {
+    name: "arbok",
+    pokedex: null,
+  },
+  "025": {
+    name: "pikachu",
+    pokedex: null,
+  },
+  "026": {
+    name: "raichu",
+    pokedex: null,
+  },
+  "027": {
+    name: "sandshrew",
+    pokedex: null,
+  },
+  "028": {
+    name: "sandslash",
+    pokedex: null,
+  },
+  "029": {
+    name: "nidoran",
+    pokedex: null,
+  },
+  "030": {
+    name: "nidorina",
+    pokedex: null,
+  },
+  "031": {
+    name: "nidoqueen",
+    pokedex: null,
+  },
+  "032": {
+    name: "nidoran",
+    pokedex: null,
+  },
+  "033": {
+    name: "nidorino",
+    pokedex: null,
+  },
+  "034": {
+    name: "nidoking",
+    pokedex: null,
+  },
+  "035": {
+    name: "clefairy",
+    pokedex: null,
+  },
+  "036": {
+    name: "clefable",
+    pokedex: null,
+  },
+  "037": {
+    name: "vulpix",
+    pokedex: null,
+  },
+  "038": {
+    name: "ninetales",
+    pokedex: null,
+  },
+  "039": {
+    name: "jigglypuff",
+    pokedex: null,
+  },
+  "040": {
+    name: "wigglytuff",
+    pokedex: null,
+  },
+  "041": {
+    name: "zubat",
+    pokedex: null,
+  },
+  "042": {
+    name: "golbat",
+    pokedex: null,
+  },
+  "043": {
+    name: "oddish",
+    pokedex: null,
+  },
+  "044": {
+    name: "gloom",
+    pokedex: null,
+  },
+  "045": {
+    name: "vileplume",
+    pokedex: null,
+  },
+  "046": {
+    name: "paras",
+    pokedex: null,
+  },
+  "047": {
+    name: "parasect",
+    pokedex: null,
+  },
+  "048": {
+    name: "venonat",
+    pokedex: null,
+  },
+  "049": {
+    name: "venomoth",
+    pokedex: null,
+  },
+  "050": {
+    name: "diglett",
+    pokedex: null,
+  },
+  "051": {
+    name: "dugtrio",
+    pokedex: null,
+  },
+  "052": {
+    name: "meowth",
+    pokedex: null,
+  },
+  "053": {
+    name: "persian",
+    pokedex: null,
+  },
+  "054": {
+    name: "psyduck",
+    pokedex: null,
+  },
+  "055": {
+    name: "golduck",
+    pokedex: null,
+  },
+  "056": {
+    name: "mankey",
+    pokedex: null,
+  },
+  "057": {
+    name: "primeape",
+    pokedex: null,
+  },
+  "058": {
+    name: "growlithe",
+    pokedex: null,
+  },
+  "059": {
+    name: "arcanine",
+    pokedex: null,
+  },
+  "060": {
+    name: "poliwag",
+    pokedex: null,
+  },
+  "061": {
+    name: "poliwhirl",
+    pokedex: null,
+  },
+  "062": {
+    name: "poliwrath",
+    pokedex: null,
+  },
+  "063": {
+    name: "abra",
+    pokedex: null,
+  },
+  "064": {
+    name: "kadabra",
+    pokedex: null,
+  },
+  "065": {
+    name: "alakazam",
+    pokedex: null,
+  },
+  "066": {
+    name: "machop",
+    pokedex: null,
+  },
+  "067": {
+    name: "machoke",
+    pokedex: null,
+  },
+  "068": {
+    name: "machamp",
+    pokedex: null,
+  },
+  "069": {
+    name: "bellsprout",
+    pokedex: null,
+  },
+  "070": {
+    name: "weepinbell",
+    pokedex: null,
+  },
+  "071": {
+    name: "victreebel",
+    pokedex: null,
+  },
+  "072": {
+    name: "tentacool",
+    pokedex: null,
+  },
+  "073": {
+    name: "tentacruel",
+    pokedex: null,
+  },
+  "074": {
+    name: "geodude",
+    pokedex: null,
+  },
+  "075": {
+    name: "graveler",
+    pokedex: null,
+  },
+  "076": {
+    name: "golem",
+    pokedex: null,
+  },
+  "077": {
+    name: "ponyta",
+    pokedex: null,
+  },
+  "078": {
+    name: "rapidash",
+    pokedex: null,
+  },
+  "079": {
+    name: "slowpoke",
+    pokedex: null,
+  },
+  "080": {
+    name: "slowbro",
+    pokedex: null,
+  },
+  "081": {
+    name: "magnemite",
+    pokedex: null,
+  },
+  "082": {
+    name: "magneton",
+    pokedex: null,
+  },
+  "083": {
+    name: "farfetch'd",
+    pokedex: null,
+  },
+  "084": {
+    name: "doduo",
+    pokedex: null,
+  },
+  "085": {
+    name: "dodrio",
+    pokedex: null,
+  },
+  "086": {
+    name: "seel",
+    pokedex: null,
+  },
+  "087": {
+    name: "dewgong",
+    pokedex: null,
+  },
+  "088": {
+    name: "grimer",
+    pokedex: null,
+  },
+  "089": {
+    name: "muk",
+    pokedex: null,
+  },
+  "090": {
+    name: "shellder",
+    pokedex: null,
+  },
+  "091": {
+    name: "cloyster",
+    pokedex: null,
+  },
+  "092": {
+    name: "gastly",
+    pokedex: null,
+  },
+  "093": {
+    name: "haunter",
+    pokedex: null,
+  },
+  "094": {
+    name: "gengar",
+    pokedex: null,
+  },
+  "095": {
+    name: "onix",
+    pokedex: null,
+  },
+  "096": {
+    name: "drowzee",
+    pokedex: null,
+  },
+  "097": {
+    name: "hypno",
+    pokedex: null,
+  },
+  "098": {
+    name: "krabby",
+    pokedex: null,
+  },
+  "099": {
+    name: "kingler",
+    pokedex: null,
+  },
+  100: {
+    name: "voltorb",
+    pokedex: null,
+  },
+  101: {
+    name: "electrode",
+    pokedex: null,
+  },
+  102: {
+    name: "exeggcute",
+    pokedex: null,
+  },
+  103: {
+    name: "exeggutor",
+    pokedex: null,
+  },
+  104: {
+    name: "cubone",
+    pokedex: null,
+  },
+  105: {
+    name: "marowak",
+    pokedex: null,
+  },
+  106: {
+    name: "hitmonlee",
+    pokedex: null,
+  },
+  107: {
+    name: "hitmonchan",
+    pokedex: null,
+  },
+  108: {
+    name: "lickitung",
+    pokedex: null,
+  },
+  109: {
+    name: "koffing",
+    pokedex: null,
+  },
+  110: {
+    name: "weezing",
+    pokedex: null,
+  },
+  111: {
+    name: "rhyhorn",
+    pokedex: null,
+  },
+  112: {
+    name: "rhydon",
+    pokedex: null,
+  },
+  113: {
+    name: "chansey",
+    pokedex: null,
+  },
+  114: {
+    name: "tangela",
+    pokedex: null,
+  },
+  115: {
+    name: "kangaskhan",
+    pokedex: null,
+  },
+  116: {
+    name: "horsea",
+    pokedex: null,
+  },
+  117: {
+    name: "seadra",
+    pokedex: null,
+  },
+  118: {
+    name: "goldeen",
+    pokedex: null,
+  },
+  119: {
+    name: "seaking",
+    pokedex: null,
+  },
+  120: {
+    name: "staryu",
+    pokedex: null,
+  },
+  121: {
+    name: "starmie",
+    pokedex: null,
+  },
+  122: {
+    name: "mr.mime",
+    pokedex: null,
+  },
+  123: {
+    name: "scyther",
+    pokedex: null,
+  },
+  124: {
+    name: "jynx",
+    pokedex: null,
+  },
+  125: {
+    name: "electabuzz",
+    pokedex: null,
+  },
+  126: {
+    name: "magmar",
+    pokedex: null,
+  },
+  127: {
+    name: "pinsir",
+    pokedex: null,
+  },
+  128: {
+    name: "tauros",
+    pokedex: null,
+  },
+  129: {
+    name: "magikarp",
+    pokedex: null,
+  },
+  130: {
+    name: "gyarados",
+    pokedex: null,
+  },
+  131: {
+    name: "lapras",
+    pokedex: null,
+  },
+  132: {
+    name: "ditto",
+    pokedex: null,
+  },
+  133: {
+    name: "eevee",
+    pokedex: null,
+  },
+  134: {
+    name: "vaporeon",
+    pokedex: null,
+  },
+  135: {
+    name: "jolteon",
+    pokedex: null,
+  },
+  136: {
+    name: "flareon",
+    pokedex: null,
+  },
+  137: {
+    name: "porygon",
+    pokedex: null,
+  },
+  138: {
+    name: "omanyte",
+    pokedex: null,
+  },
+  139: {
+    name: "omastar",
+    pokedex: null,
+  },
+  140: {
+    name: "kabuto",
+    pokedex: null,
+  },
+  141: {
+    name: "kabutops",
+    pokedex: null,
+  },
+  142: {
+    name: "aerodactyl",
+    pokedex: null,
+  },
+  143: {
+    name: "snorlax",
+    pokedex: null,
+  },
+  144: {
+    name: "articuno",
+    pokedex: null,
+  },
+  145: {
+    name: "zapdos",
+    pokedex: null,
+  },
+  146: {
+    name: "moltres",
+    pokedex: null,
+  },
+  147: {
+    name: "dratini",
+    pokedex: null,
+  },
+  148: {
+    name: "dragonair",
+    pokedex: null,
+  },
+  149: {
+    name: "dragonite",
+    pokedex: null,
+  },
+  150: {
+    name: "mewtwo",
+    pokedex: null,
+  },
+  151: {
+    name: "mew",
+    pokedex: null,
+  },
+  152: {
+    name: "Chikorita",
+    pokedex: null,
+  },
+  153: {
+    name: "Bayleef",
+    pokedex: null,
+  },
+  154: {
+    name: "Meganium",
+    pokedex: null,
+  },
+  155: {
+    name: "Cyndaquil",
+    pokedex: null,
+  },
+  156: {
+    name: "Quilava",
+    pokedex: null,
+  },
+  157: {
+    name: "Typhlosion",
+    pokedex: null,
+  },
+  158: {
+    name: "Totodile",
+    pokedex: null,
+  },
+  159: {
+    name: "Croconaw",
+    pokedex: null,
+  },
+  160: {
+    name: "Feraligatr",
+    pokedex: null,
+  },
+  161: {
+    name: "Sentret",
+    pokedex: null,
+  },
+  162: {
+    name: "Furret",
+    pokedex: null,
+  },
+  163: {
+    name: "Hoothoot",
+    pokedex: null,
+  },
+  164: {
+    name: "Noctowl",
+    pokedex: null,
+  },
+  165: {
+    name: "Ledyba",
+    pokedex: null,
+  },
+  166: {
+    name: "Ledian",
+    pokedex: null,
+  },
+  167: {
+    name: "Spinarak",
+    pokedex: null,
+  },
+  168: {
+    name: "Ariados",
+    pokedex: null,
+  },
+  169: {
+    name: "Crobat",
+    pokedex: null,
+  },
+  170: {
+    name: "Chinchou",
+    pokedex: null,
+  },
+  171: {
+    name: "Lanturn",
+    pokedex: null,
+  },
+  172: {
+    name: "Pichu",
+    pokedex: null,
+  },
+  173: {
+    name: "Cleffa",
+    pokedex: null,
+  },
+  174: {
+    name: "Igglybuff",
+    pokedex: null,
+  },
+  175: {
+    name: "Togepi",
+    pokedex: null,
+  },
+  176: {
+    name: "Togetic",
+    pokedex: null,
+  },
+  177: {
+    name: "Natu",
+    pokedex: null,
+  },
+  178: {
+    name: "Xatu",
+    pokedex: null,
+  },
+  179: {
+    name: "Mareep",
+    pokedex: null,
+  },
+  180: {
+    name: "Flaaffy",
+    pokedex: null,
+  },
+  181: {
+    name: "Ampharos",
+    pokedex: null,
+  },
+  182: {
+    name: "Bellossom",
+    pokedex: null,
+  },
+  183: {
+    name: "Marill",
+    pokedex: null,
+  },
+  184: {
+    name: "Azumarill",
+    pokedex: null,
+  },
+  185: {
+    name: "Sudowoodo",
+    pokedex: null,
+  },
+  186: {
+    name: "Politoed",
+    pokedex: null,
+  },
+  187: {
+    name: "Hoppip",
+    pokedex: null,
+  },
+  188: {
+    name: "Skiploom",
+    pokedex: null,
+  },
+  189: {
+    name: "Jumpluff",
+    pokedex: null,
+  },
+  190: {
+    name: "Aipom",
+    pokedex: null,
+  },
+  191: {
+    name: "Sunkern",
+    pokedex: null,
+  },
+  192: {
+    name: "Sunflora",
+    pokedex: null,
+  },
+  193: {
+    name: "Yanma",
+    pokedex: null,
+  },
+  194: {
+    name: "Wooper",
+    pokedex: null,
+  },
+  195: {
+    name: "Quagsire",
+    pokedex: null,
+  },
+  196: {
+    name: "Espeon",
+    pokedex: null,
+  },
+  197: {
+    name: "Umbreon",
+    pokedex: null,
+  },
+  198: {
+    name: "Murkrow",
+    pokedex: null,
+  },
+  199: {
+    name: "Slowking",
+    pokedex: null,
+  },
+  200: {
+    name: "Misdreavus",
+    pokedex: null,
+  },
+  201: {
+    name: "Unown",
+    pokedex: null,
+  },
+  202: {
+    name: "Wobbuffet	",
+    pokedex: null,
+  },
+  203: {
+    name: "Girafarig	",
+    pokedex: null,
+  },
+  204: {
+    name: "Pineco",
+    pokedex: null,
+  },
+  205: {
+    name: "Forretress",
+    pokedex: null,
+  },
+  206: {
+    name: "Dunsparce",
+    pokedex: null,
+  },
+  207: {
+    name: "Gligar",
+    pokedex: null,
+  },
+  208: {
+    name: "Steelix",
+    pokedex: null,
+  },
+  209: {
+    name: "Snubbull",
+    pokedex: null,
+  },
+  210: {
+    name: "Granbull",
+    pokedex: null,
+  },
+  211: {
+    name: "Qwilfish",
+    pokedex: null,
+  },
+  212: {
+    name: "Scizor",
+    pokedex: null,
+  },
+  213: {
+    name: "Shuckle",
+    pokedex: null,
+  },
+  214: {
+    name: "Heracross",
+    pokedex: null,
+  },
+  215: {
+    name: "Sneasel",
+    pokedex: null,
+  },
+  216: {
+    name: "Teddiursa",
+    pokedex: null,
+  },
+  217: {
+    name: "Ursaring",
+    pokedex: null,
+  },
+  218: {
+    name: "Slugma",
+    pokedex: null,
+  },
+  219: {
+    name: "Magcargo",
+    pokedex: null,
+  },
+  220: {
+    name: "Swinub",
+    pokedex: null,
+  },
+  221: {
+    name: "Piloswine",
+    pokedex: null,
+  },
+  222: {
+    name: "Corsola",
+    pokedex: null,
+  },
+  223: {
+    name: "Remoraid",
+    pokedex: null,
+  },
+  224: {
+    name: "Octillery	",
+    pokedex: null,
+  },
+  225: {
+    name: "Delibird",
+    pokedex: null,
+  },
+  226: {
+    name: "Mantine",
+    pokedex: null,
+  },
+  227: {
+    name: "Skarmory",
+    pokedex: null,
+  },
+  228: {
+    name: "Houndour",
+    pokedex: null,
+  },
+  229: {
+    name: "Houndoom",
+    pokedex: null,
+  },
+  230: {
+    name: "Kingdra",
+    pokedex: null,
+  },
+  231: {
+    name: "Phanpy",
+    pokedex: null,
+  },
+  232: {
+    name: "Donphan",
+    pokedex: null,
+  },
+  233: {
+    name: "Porygon2",
+    pokedex: null,
+  },
+  234: {
+    name: "Stantler",
+    pokedex: null,
+  },
+  235: {
+    name: "Smeargle",
+    pokedex: null,
+  },
+  236: {
+    name: "Tyrogue",
+    pokedex: null,
+  },
+  237: {
+    name: "Hitmontop",
+    pokedex: null,
+  },
+  238: {
+    name: "Smoochum",
+    pokedex: null,
+  },
+  239: {
+    name: "Elekid",
+    pokedex: null,
+  },
+  240: {
+    name: "Magby",
+    pokedex: null,
+  },
+  241: {
+    name: "Miltank",
+    pokedex: null,
+  },
+  242: {
+    name: "Blissey",
+    pokedex: null,
+  },
+  243: {
+    name: "Raikou",
+    pokedex: null,
+  },
+  244: {
+    name: "Entei",
+    pokedex: null,
+  },
+  245: {
+    name: "Suicune",
+    pokedex: null,
+  },
+  246: {
+    name: "Larvitar",
+    pokedex: null,
+  },
+  247: {
+    name: "Pupitar",
+    pokedex: null,
+  },
+  248: {
+    name: "Tyranitar",
+    pokedex: null,
+  },
+  249: {
+    name: "Lugia",
+    pokedex: null,
+  },
+  250: {
+    name: "Ho-oh",
+    pokedex: null,
+  },
+  251: {
+    name: "Celebi",
+    pokedex: null,
+  },
+  252: {
+    name: "Treecko",
+    pokedex: null,
+  },
+};
---
+++ wtp/wtp.js
@@ -0,0 +1,185 @@
+let mute = false;
+let auto = true;
+let per = "All";
+let language = "en";
+let botuser = "";
+let pokemon;
+let pokeDex = "true";
+let heightTxt = "Height";
+let weightTxt = "Weight";
+let foundByTxt = "Found By";
+let wasRightTxt = "You Was Right";
+
+let autoWtp = false;
+let isWtpSolved = false;
+let isRunning = false;
+let min = 1;
+let max = 151;
+
+let resource;
+let ext;
+let rType = "animated";
+let cry = true;
+let pokemonID;
+let tShow = true;
+
+// Mine
+let pokeNum = 0;
+let currentName = "";
+let currentPokemon = {};
+
+function getRandomNumber(max) {
+  let randNumber = Math.floor(Math.random() * max) + 1;
+  var amountOfZeros = getAmoutOfZeros(randNumber);
+  return amountOfZeros + randNumber;
+}
+
+function getAmoutOfZeros(number) {
+  if (number < 10) {
+    return "00";
+  } else if (number >= 10 && number < 100) {
+    return "0";
+  } else {
+    return "";
+  }
+}
+
+function startWtp(command) {
+  if (isRunning) resetWtp();
+
+  isWtpSolved = false;
+  isRunning = true;
+
+  if (rType === "original") {
+    resource = "original";
+    ext = "png";
+  } else {
+    resource = "animated";
+    ext = "gif";
+  }
+
+  if (command == "wtp") {
+    pokeNum = getRandomNumber(151);
+  }
+  if (command == "wtp2") {
+    pokeNum = getRandomNumber(252);
+  }
+
+  currentName = pokemonList[pokeNum].name.toLowerCase().replaceAll(/\s/g, "");
+  currentPokemon = pokemonList[pokeNum];
+  console.log(currentName);
+
+  document.getElementById("ball").style.visibility = "visible";
+
+  document.getElementById("ballVid").play();
+  if (!mute) {
+    document.getElementById("who-that-pokemon-audio").play();
+  }
+}
+
+function stopVid() {
+  document.getElementById("ball").style.visibility = "hidden";
+  showBlurredImage();
+  console.log("HIDE");
+}
+
+function showBlurredImage() {
+  let dittoNumber = 0;
+  if(pokeNum == 132) {
+    if (command == "wtp") {
+      dittoNumber = getRandomNumber(151);
+    }
+    if (command == "wtp2") {
+      dittoNumber = getRandomNumber(252);
+    }
+  }
+  let blurredImage = document.createElement("img");
+  if(pokeNum == 132) {
+    blurredImage.src = "./wtp/assets/pokemons/" + dittoNumber + ".gif";
+  } else {
+    blurredImage.src = "./wtp/assets/pokemons/" + pokeNum + ".gif";
+  }
+  blurredImage.className = "blurred";
+  blurredImage.id = "pokemon-image";
+
+  let parentElement = document.getElementById("video-holder");
+  parentElement.appendChild(blurredImage);
+}
+
+function giveUp() {
+  guess(currentName);
+}
+
+function skipWtp() {
+  resetWtp();
+  startGame();
+}
+
+// message, user
+function guess(x, n) {
+  // x = guessedName
+  // n = usename of user that guessed
+  https: console.log(x);
+  console.log(n);
+  console.log("currentName");
+  console.log(currentName);
+  let guessedName = x.toLowerCase().replaceAll(/\s/g, "");
+  console.log("guessedName");
+  console.log(guessedName);
+  if (guessedName == currentName) {
+    isWtpSolved = true;
+    currentName = "";
+    document.getElementById("pokemon-image").className = "";
+    ComfyJS.Say("Good job " + n + ". You guessed that it was " + guessedName);
+    if (currentPokemon.pokedex != null) {
+      ComfyJS.Say("Pokédex: " + currentPokemon.pokedex);
+    }
+    if (wtpCorrectWebhook != null) {
+      fetch(wtpCorrectWebhook, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: n,
+          iswtproyale: false,
+        }),
+      });
+    }
+    setTimeout(function () {
+      resetWtp();
+    }, 3000);
+  }
+}
+
+function resetWtp() {
+  if (autoWtp) {
+    document.getElementById("pokemon-image").remove();
+    startWtp("wtp");
+  } else {
+    isWtpSolved = false;
+    isRunning = false;
+    gameRunning = false;
+    document.getElementById("pokemon-image").remove();
+  }
+}
+
+function stopGame() {
+  location.reload();
+}
+
+let autoStart = false;
+
+if (autoStart) {
+  startGame();
+}
+
+function stopAuto() {
+  auto = false;
+  resetWtp();
+}
+
+function startAuto() {
+  auto = true;
+  startGame();
+}
